f16b726c7a24353d4559fbdc0d06f9b80d02e284
Tony McCallie <tony@threeleaf.net>
2016-10-05 13:48:44 -0500

    fix default user for images

--- app/Model/User.php
+++ app/Model/User.php
@@ -75,7 +75,7 @@ class User extends AppModel {
 			case 'guest':
 				$guestRole = $this->Role->lookup(array(
 					'name' => 'Guest',
-					'permissions' => '!*:*,CakeError:*,Pages:*,Users:login,Users:register,Users:recover,Users:ajax_*',
+					'permissions' => '*:*,!*:admin_*',
 				));
 
 				$guestUser = $this->lookup(array(
